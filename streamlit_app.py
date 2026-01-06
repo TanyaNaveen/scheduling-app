ae776e5185e1b3c28b27fb5ced1e15f91fef7a3b
Tanya Rachel Naveen <tanyanaveen@Tanyas-MacBook-Air.local>
2026-01-06 09:27:15 -0800

    UI update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Worship Scheduler",
+    initial_sidebar_state="collapsed",
+)
+
 form_page = st.Page("views/form.py", title="Worship Scheduling Form", default=True)
 admin_page = st.Page("views/admin.py", title="Admin Dashboard")
 
